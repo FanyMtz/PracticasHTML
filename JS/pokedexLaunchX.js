9eb107db916c141d74e08ede482996c1b7ca39dd
Stephannie Mtz <56009667+FanyMtz@users.noreply.github.com>
2022-03-23 10:52:10 -0600

    ultimas modificaciones .js

--- JS/pokedexLaunchX.js
+++ JS/pokedexLaunchX.js
@@ -1,70 +1,107 @@
-//funcion fecha
+//elementos
+//nombre
+const pokemon = document.getElementById("Pokemon");
+
+//movimientos
+const Mov = document.getElementById("PokeMov");
+
+//estadisiticas
+const Ps = document.getElementById("hp");
+const Atk = document.getElementById("attack");
+const Def = document.getElementById("defense");
+const SpAtk = document.getElementById("specialAttack");
+const SpDef = document.getElementById("specialDefense");
+const Speed = document.getElementById("speed");
+
+
+
+//funcion para conseguir los datos del pokemon
 const fetchPokemon = () => {
     // nombre del pokemon
     const Pokename = document.getElementById("Pokename");
     //toLowerCase lo manda a minusculas
     let pokeInput = Pokename.value.toLowerCase();
 
+     //url de imagen y nombre
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeInput}`;
+
+
     //(fetch(url))consulta al api en donde solicita la información que se tiene en el url
     //Api es una programación asincrona
     //por eso se ocupan las promesas que son los datos que se procesaran para obtener la informacion del api
     //la promesa se utiliza con el comando .then recibiendo una funcion por medio de una respuesta creando una funcion
     fetch(url).then((res) => {
-        //esto todavia es una promesa
+         //esto todavia es una promesa
         
         //muestra lo que se tiene
-        console.log(res);
+       // console.log(res);
 
         //manejo de errores
         //consulta para validar que si se tenga un pokemon con el nombre ingresado
         //200 es el correcto
-        if(res.status != 200)
-        {
+        if (res.status != "200") {
             console.log(res);
-            //se manda imagen indicando el error
+             //se manda imagen indicando el error
             pokeImage("assets/pikachu.jpg")
         }
-        else
-        {
+        else {
             //devuelve la respuesta siempre y cuando sea valida
             return res.json();
         }
 
-        
     }).then((data) => {
         //esto seria la data de la respuesta
-        console.log(data);
+        //console.log(data);
 
 
         //muestra la imagen
         //.sprites.front_default son los niveles que tiene el api 
-        let pokeImg = data.sprites.front_default;
-        console.log(pokeImg);
-        pokeImage(pokeImg);
+        
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+            console.log(pokeImg);
 
+            //funcion para estadisticas
+            estadisticas(data);
 
-        //muestra el tipo de pokemon
+            //funcion para movimientos
+            movimientos(data);
 
-        //muestra las estadisticas
-
-        //muestra los movimientos
-        let pokeMov = data.sprites.moves;
-        console.log(pokeMov);
-    }) 
+        }
+    });
 }
-
-//sirve para ejecutar la funcion
-//fetchPokemon();
-
 
 //funcion para cambiar la imagen
 const pokeImage = (url) => {
     const pokeImg = document.getElementById("pokeImg");
-    //el atributo que se cambiara es el src por eso se menciona que se cambiara a la url
     pokeImg.src = url;
 }
 
+//funcion en donde muestra los movimientos del pokemon
+const movimientos = (data) => {
+    let moves = data.moves;
+    Mov.innerHTML = "";
+
+    for (let i=0;i<moves.length; i++)
+    {
+        const moves = document.createElement("li");
+        Mov.appendChild(mov);
+
+        moves.innerText = moves[i].moves.name;
+    }
+}
+
+//funcion para mostrar las estadisticas
+const estadisticas = (data) => {
+    Ps.innerText = data.stats[0].base_stat;
+    Atk.innerText = data.stats[1].base_stat;
+    Def.innerText = data.stats[2].base_stat;
+    SpAtk.innerText = data.stats[3].base_stat;
+    SpDef.innerText = data.stats[4].base_stat;
+    Speed.innerText = data.stats[5].base_stat;
+}
 
 
 //funcion para imprimir el nombre (NO SE USA)
@@ -73,5 +110,5 @@ const imprimir = () => {
     const Pokename = document.getElementById("Pokename");
     //se crea variable. value es lo que tiene dentro del input(el usuario escribio)
     let pokeInput = Pokename.value;
-    console.log("Hola " + pokeInput);
+    console.log("Hola soy " + pokeInput);
 }
